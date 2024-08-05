7c7bdd8e4f16bc1d1bb81a75d411f14148abf532
José Sánchez-Gallego <gallegoj@uw.edu>
2024-08-05 08:08:31 -0700

    Improve layout of the overview page

--- app/overview/page.tsx
+++ app/overview/page.tsx
@@ -5,25 +5,56 @@
  *  @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
  */
 
+'use client';
+
 import ActorsTable from '@/src/components/APITables/ActorsTable/ActorsTable';
 import EnclosureTable from '@/src/components/APITables/EnclosureTable/EnclosureTable';
 import EphemerisTable from '@/src/components/APITables/EphemerisTable/EphemerisTable';
 import SpecTable from '@/src/components/APITables/SpecTable/SpecTable';
 import WeatherTable from '@/src/components/APITables/WeatherTable/WeatherTable';
-import { SimpleGrid, Stack } from '@mantine/core';
+import { Box, SimpleGrid, Stack, useMatches } from '@mantine/core';
+import React from 'react';
 
 export default function OverviewPage() {
+  const nCols = useMatches({ md: 1, lg: 2, xl: 3 });
+
+  let tableArrangement: React.ReactNode;
+
+  if (nCols <= 2) {
+    tableArrangement = (
+      <>
+        <Stack gap="lg">
+          <SpecTable />
+          <WeatherTable />
+          <ActorsTable />
+        </Stack>
+        <Stack gap="lg">
+          <EnclosureTable />
+          <EphemerisTable />
+        </Stack>
+      </>
+    );
+  } else {
+    tableArrangement = (
+      <>
+        <Stack gap="lg">
+          <SpecTable />
+          <WeatherTable />
+        </Stack>
+        <Stack gap="lg">
+          <EnclosureTable />
+          <EphemerisTable />
+        </Stack>
+        <ActorsTable />
+      </>
+    );
+  }
+
   return (
-    <SimpleGrid cols={{ sm: 1, md: 2, xl: 3 }} spacing="lg" verticalSpacing="lg">
-      <Stack gap="lg">
-        <SpecTable />
-        <WeatherTable />
-      </Stack>
-      <Stack gap="lg">
-        <EnclosureTable />
-        <EphemerisTable />
-      </Stack>
-      <ActorsTable />
-    </SimpleGrid>
+    <Box style={{ display: 'flex', justifyContent: 'center' }}>
+      <SimpleGrid cols={nCols} spacing="lg" verticalSpacing="lg" maw={1600}>
+        {tableArrangement}
+      </SimpleGrid>
+    </Box>
   );
 }
