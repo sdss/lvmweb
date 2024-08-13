287ad30d32deabcf6357f309e2e53134079ae464
José Sánchez-Gallego <gallegoj@uw.edu>
2024-08-13 13:11:53 -0700

    Fix eslint config to use __dirname

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,9 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 module.exports = {
   extends: ['mantine', 'plugin:@next/next/recommended', 'plugin:jest/recommended'],
   plugins: ['testing-library', 'jest'],
