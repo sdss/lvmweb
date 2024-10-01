11236f49a6aff9f326ab1340818e3a8102e2f851
José Sánchez-Gallego <gallegoj@uw.edu>
2024-09-30 17:23:12 -0700

    Automatically reload page once a day

--- src/components/LVMWebRoot/LVMWebRoot.tsx
+++ src/components/LVMWebRoot/LVMWebRoot.tsx
@@ -7,9 +7,9 @@
 
 'use client';
 
+import React from 'react';
 import useAlerts, { AlertsModel } from '@/src/hooks/use-alerts';
 import useIsLogged from '@/src/hooks/use-is-logged';
-import React from 'react';
 import LVMAppShell from './LVMAppShell/LVMAppShell';
 
 export const AlertsContext = React.createContext<AlertsModel | undefined>(undefined);
@@ -26,6 +26,16 @@ type AuthContextType = {
 export default function LVMWebRoot({ children }: { children: React.ReactNode }) {
   const alerts = useAlerts();
   const [isLogged, checkLogged] = useIsLogged();
+
+  React.useEffect(() => {
+    // Force a daily reload of the entire site.
+
+    const interval = setInterval(
+      global.window.location.reload.bind(window.location),
+      86400000
+    );
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <AlertsContext.Provider value={alerts}>
