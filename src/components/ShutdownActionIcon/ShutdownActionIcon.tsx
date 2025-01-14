3c582781013bfd31bd60ad2ce91c867c0518829c
José Sánchez-Gallego <gallegoj@uw.edu>
2025-01-14 11:22:36 -0300

    Disable shutdown button when not logged in or not at LCO

--- src/components/ShutdownActionIcon/ShutdownActionIcon.tsx
+++ src/components/ShutdownActionIcon/ShutdownActionIcon.tsx
@@ -14,18 +14,28 @@ import { useDisclosure } from '@mantine/hooks';
 import { isLCO } from '@/src/actions/get-ip';
 import useTask from '@/src/hooks/use-task';
 import ConfirmationModal from '../ConfirmationModal/ConfirmationModal';
+import { AuthContext } from '../LVMWebRoot/LVMWebRoot';
 import classes from './ShutdownActionIcon.module.css';
 
 export default function ShutdownActionIcon() {
   const [isOpen, { open, close }] = useDisclosure(false);
   const [runner, isRunning] = useTask({ taskName: 'shutdown', notifyErrors: false });
 
+  const [atLCO, setAtLCO] = React.useState(false);
+  const authStatus = React.useContext(AuthContext);
+
+  React.useEffect(() => {
+    isLCO()
+      .then((value) => setAtLCO(value))
+      .catch(() => setAtLCO(false));
+  }, []);
+
   const shutDown = React.useCallback(async () => {
     close();
 
     const lcoOverride = process.env.LCO_OVERRIDE_CODE;
 
-    if ((await isLCO()) && lcoOverride !== undefined) {
+    if (!authStatus.logged && atLCO && lcoOverride !== undefined) {
       runner(
         `/macros/shutdownLCO?override_code=${lcoOverride}&disable_overwatcher=1`,
         true
@@ -33,9 +43,10 @@ export default function ShutdownActionIcon() {
     } else {
       runner('/macros/shutdown?disable_overwatcher=1', true).catch(() => {});
     }
-  }, [close, runner]);
+  }, [close, runner, atLCO, authStatus.logged]);
 
   const label = isRunning ? 'Emergency shutdown in progress' : 'Emergency shutdown';
+  const disabled = isRunning || (!atLCO && !authStatus.logged);
 
   return (
     <>
@@ -51,7 +62,10 @@ export default function ShutdownActionIcon() {
             color="white"
             classNames={{ icon: classes.icon }}
             onClick={open}
-            disabled={isRunning}
+            disabled={disabled}
+            style={{
+              backgroundColor: disabled ? 'transparent' : 'unset',
+            }}
             variant="transparent"
           >
             {isRunning ? (
@@ -60,7 +74,7 @@ export default function ShutdownActionIcon() {
               <IconArrowBarLeft />
             )}
           </ActionIcon>
-          <Text size="sm" c="gray.2" visibleFrom="sm">
+          <Text size="sm" c={disabled ? 'dimmed' : 'gray.2'} visibleFrom="sm">
             Shutdown
           </Text>
         </Group>
