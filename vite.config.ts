bf24933ea17ad72973b75038be0c2d11df7923fc
José Sánchez-Gallego <gallegoj@uw.edu>
2026-09-13 12:27:37 -0700

    Migrate to using Vite+ and update deps, including TSC 7

---
+++ vite.config.ts
@@ -0,0 +1,54 @@
+import { defineConfig } from 'vite-plus';
+
+export default defineConfig({
+  staged: {
+    '*': 'vp check --fix',
+  },
+  lint: {
+    plugins: ['oxc', 'typescript', 'unicorn', 'react'],
+    categories: {
+      correctness: 'warn',
+    },
+    env: {
+      builtin: true,
+    },
+    options: {
+      typeAware: true,
+      typeCheck: true,
+    },
+    jsPlugins: [
+      {
+        name: 'vite-plus',
+        specifier: 'vite-plus/oxlint-plugin',
+      },
+    ],
+    rules: {
+      'vite-plus/prefer-vite-plus-imports': 'error',
+    },
+  },
+  fmt: {
+    printWidth: 88,
+    singleQuote: true,
+    trailingComma: 'es5',
+    importOrder: [
+      '.*styles.css$',
+      '',
+      'dayjs',
+      '^react$',
+      '^next$',
+      '^next/.*$',
+      '<BUILTIN_MODULES>',
+      '<THIRD_PARTY_MODULES>',
+      '^@mantine/(.*)$',
+      '^@mantinex/(.*)$',
+      '^@mantine-tests/(.*)$',
+      '^@docs/(.*)$',
+      '^@/.*$',
+      '^../(?!.*.css$).*$',
+      '^./(?!.*.css$).*$',
+      '\\.css$',
+    ],
+    sortPackageJson: false,
+    ignorePatterns: ['.next'],
+  },
+});
